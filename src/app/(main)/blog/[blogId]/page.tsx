9d3c061370bae143d715ebd5f4892ff51d5012c7
Vishal Kumar <kumarvishal823003@gmail.com>
2024-07-29 09:45:11 +0530

    Added novel editor

--- src/app/(main)/blog/[blogId]/page.tsx
+++ src/app/(main)/blog/[blogId]/page.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+"use client"
+import Editor from '@/components/BlogEditor/Editor'
+import React, { useEffect, useState } from 'react'
 
 type props = {}
 
+export const defaultValue = {
+  "type": "doc",
+  "content": [
+    {
+      "type": "paragraph",
+      "content": [
+        {
+          "type": "text",
+          "text": "Wow, this editor instance exports its content as JSON."
+        }
+      ]
+    }
+  ]
+}
+
 const page = (props: props) => {
+
+  const [content, setContent] = useState('')
+
+  useEffect(() => {
+    console.log(content)
+  }, [content, setContent])
+
   return (
     <div>
-      page
+      <Editor initialValue={defaultValue} onChange={setContent} />
     </div>
   )
 }
