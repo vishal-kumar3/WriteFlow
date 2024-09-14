9bbeac71a978fc4cc95adf8b683db889e5813086
Vishal Kumar <kumarvishal823003@gmail.com>
2024-09-15 00:23:16 +0530

    user page responsive

--- src/components/UserProfile/AvatarImage.tsx
+++ src/components/UserProfile/AvatarImage.tsx
@@ -17,10 +17,10 @@ const AvatarImage = async ({ avatarImage, userId, followingCnt, followerCnt, use
   // TODO: user.action.ts me daal do or without login page access ho jaye vaise kr do
 
   return (
-    <div className="flex flex-col items-center">
-      <div className="relative sm:static">
+    <div className=" flex flex-col items-center sm:items-start sm:flex-row sm:justify-between px-4 sm:px-8 -mt-16 sm:-mt-20 relative z-10">
+      <div className="relative">
         <Image
-          className="rounded-full size-[220px] object-cover absolute bottom-0 sm:top-[30%] sm:left-[8%]"
+          className="rounded-full w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 object-cover border-4 border-white dark:border-gray-800"
           src={avatarImage}
           width={220}
           height={220}
@@ -28,7 +28,7 @@ const AvatarImage = async ({ avatarImage, userId, followingCnt, followerCnt, use
         />
         {/* @ts-expect-error Async Server Component */}
         <CurrentUserOnly userId={userId}>
-          <div className="sm:absolute sm:top-[90%] sm:left-[7%]">
+          <div className="absolute bottom-0 right-0">
             <FileUploader
               ctx_name="AvatarImage"
               userId={userId!}
@@ -38,9 +38,11 @@ const AvatarImage = async ({ avatarImage, userId, followingCnt, followerCnt, use
           </div>
         </CurrentUserOnly>
       </div>
-      <div className="flex text-lg gap-8 items-center mt-2 lg:ml-[calc(220px+8%)]">
-        <p>{followerCnt} Followers</p>
-        <p>{followingCnt} Followings</p>
+      <div className="mt-4 sm:self-center flex flex-col sm:flex-row items-center sm:items-end gap-4">
+        <div className="text-center sm:flex sm:gap-2 sm:text-right">
+          <p className="text-lg font-semibold">{followerCnt} Followers</p>
+          <p className="text-lg font-semibold">{followingCnt} Following</p>
+        </div>
         {/* @ts-expect-error Async Server Component */}
         <HideForCurrentUser userId={userId} >
           <FollowButtonServerWraper username={username} id={userId} />
